d5c0209afbf62aabda7770f7297b276c0cb3b9aa
OsyelOrozco <109883787+OsyelOrozco@users.noreply.github.com>
2024-04-15 19:31:41 -0600

    links
    se corrigieron los likns

--- roles.js
+++ roles.js
@@ -3,11 +3,11 @@ function ingresarComo(rol) {
     if (rol === 'administrador') {
         window.location.href = 'administrador/index.html';
     } else if (rol === 'usuario') {
-        window.location.href = 'user/usuario.php';
+        window.location.href = 'user/usuario.html';
     } else if (rol === 'enfermera(o)') {
-        window.location.href = 'enfer/enfermera.php';
+        window.location.href = 'enfermera/index.html';
     } else if (rol === 'doctor') {
-        window.location.href = 'docto/doctor.php';
+        window.location.href = 'Doctor/index.html';
     }
 }
 
